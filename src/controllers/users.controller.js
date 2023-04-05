a033926f406fb8fa73158f4ee7803056a08a1dc0
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-04-06 00:25:18 +0700

    add error handling in deleteUser

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -9,23 +9,56 @@ exports.getAllUsers = async (req, res) => {
   });
 };
 
-exports.createUser = (req, res) => {
-  return res.json({
-    success: true,
-    message: `Create user ${req.body.fullName} successfully`,
+exports.getOneUser = async (req, res) => {
+  const data = await userModel.findOne(req.params.id);
+  if (data) {
+    return res.json({
+      success: true,
+      message: "Detail user",
+      results: data,
+    });
+  }
+  return res.status(404).json({
+    success: false,
+    message: "Error: User not found",
   });
 };
 
-exports.updateUser = (req, res) => {
+exports.createUser = async (req, res) => {
+  const data = await userModel.insert(req.params.id, req.body);
+  if (!data) {
+    return res.status(404).json({
+      success: false,
+      message: "Error: User is already exists",
+    });
+  }
   return res.json({
     success: true,
-    message: `Update user ${req.params.id} successfully`,
+    message: `Create user ${req.body.email} successfully`,
+    results: data,
   });
 };
 
-exports.deleteUser = (req, res) => {
+exports.updateUser = async (req, res) => {
+  const data = await userModel.update(req.params.id, req.body);
   return res.json({
     success: true,
-    message: `Delete user ${req.params.id} successfully`,
+    message: "Update user successfully",
+    results: data,
+  });
+};
+
+exports.deleteUser = async (req, res) => {
+  const data = await userModel.destroy(req.params.id);
+  if (data) {
+    return res.json({
+      success: true,
+      message: "Delete user successfully",
+      results: data,
+    });
+  }
+  return res.status(404).json({
+    success: false,
+    message: "Error: User not found",
   });
 };
