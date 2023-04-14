b3af8a77b9951811b538a4defbd695e2003c71d1
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-04-14 20:42:51 +0700

    Updating sql syntax

--- base.sql
+++ base.sql
@@ -1,30 +1,112 @@
 CREATE TABLE "users" (
-  "id" INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  "fullName" VARCHAR(80),
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "username" VARCHAR(255),
   "email" VARCHAR(255) UNIQUE,
   "password" VARCHAR(255),
   "createdAt" TIMESTAMP DEFAULT NOW(),
-  "updatedAt" TIMESTAMP NULL
+  "updatedAt" TIMESTAMP DEFAULT NULL
 );
 
-INSERT INTO "users" ("email", "password") VALUES ('admin@mail.com', '1234');
-INSERT INTO "users" ("email", "password") VALUES ('fazztrack@mail.com', '1234');
-INSERT INTO "users" ("email", "password") VALUES ('ronald@mail.com', '1234');
-INSERT INTO "users" ("fullName", "email", "password") VALUES 
-('Rick', 'rick@mail.com', '1234'),
-('Antonio', 'antonio@mail.com', '1234'),
-('Jack', 'jack@mail.com', '1234'),
-('Roy', 'roy@mail.com', '1234'),
-('Alan', 'alan@mail.com', '1234');
+CREATE TABLE "profile" (
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "picture" VARCHAR(255),
+  "fullName" VARCHAR(255),
+  "phoneNumber" VARCHAR(255),
+  "gender" BOOLEAN,
+  "profession" VARCHAR(255),
+  "nasionality" VARCHAR(255),
+  "birthDate" DATE,
+  "createdAt" TIMESTAMP DEFAULT NOW(),
+  "updatedAt" TIMESTAMP DEFAULT NULL
+);
 
-UPDATE "users" SET "email"='admin@gmail.com' WHERE "id"=1;
+CREATE TABLE "categories" (
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "name" VARCHAR(255),
+  "createdAt" TIMESTAMP DEFAULT NOW(),
+  "updatedAt" TIMESTAMP DEFAULT NULL
+);
 
-DELETE FROM "users" WHERE "id"=2;
+CREATE TABLE "cities" (
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "picture" VARCHAR(255),
+  "name" VARCHAR(255),
+  "createdAt" TIMESTAMP DEFAULT NOW(),
+  "updatedAt" TIMESTAMP DEFAULT NULL
+);
 
-SELECT * FROM "users" LIMIT 5 OFFSET 0;
+CREATE TABLE "events" (
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "picture" VARCHAR(255),
+  "title" VARCHAR(255),
+  "date" DATE,
+  "cityId" INTEGER,
+  "descriptions" TEXT,
+  "createdAt" TIMESTAMP DEFAULT NOW(),
+  "updatedAt" TIMESTAMP DEFAULT NULL
+);
 
-SELECT * FROM "users" ORDER BY "email" ASC LIMIT 5 OFFSET 0;
+CREATE TABLE "eventCategories" (
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "eventId" INTEGER,
+  "categoryId" INTEGER,
+  "createdAt" TIMESTAMP DEFAULT NOW(),
+  "updatedAt" TIMESTAMP DEFAULT NULL
+);
 
-DROP TABLE "users";
+CREATE TABLE "partners" (
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "picture" VARCHAR(255),
+  "name" VARCHAR(255),
+  "createdAt" TIMESTAMP DEFAULT NOW(),
+  "updatedAt" TIMESTAMP DEFAULT NULL
+);
 
-ALTER TABLE "users" ADD COLUMN "picture" VARCHAR(255);
+CREATE TABLE "reservationSections" (
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "name" VARCHAR(255),
+  "price" VARCHAR(255),
+  "createdAt" TIMESTAMP DEFAULT NOW(),
+  "updatedAt" TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE "reservationStatus" (
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "name" VARCHAR(255),
+  "createdAt" TIMESTAMP DEFAULT NOW(),
+  "updatedAt" TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE "paymentMethods" (
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "name" VARCHAR(255),
+  "createdAt" TIMESTAMP DEFAULT NOW(),
+  "updatedAt" TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE "reservations" (
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "eventId" INTEGER,
+  "userId" INTEGER,
+  "statusId" INTEGER,
+  "paymentMethodId" INTEGER,
+  "createdAt" TIMESTAMP DEFAULT NOW(),
+  "updatedAt" TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE "reservationTickets" (
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "reservationId" INTEGER,
+  "sectionId" INTEGER,
+  "quantity" INTEGER,
+  "createdAt" TIMESTAMP DEFAULT NOW(),
+  "updatedAt" TIMESTAMP DEFAULT NULL
+);
+
+CREATE TABLE "wishlists" (
+  "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  "eventId" INTEGER,
+  "userId" INTEGER,
+  "createdAt" TIMESTAMP DEFAULT NOW(),
+  "updatedAt" TIMESTAMP DEFAULT NULL
+);
