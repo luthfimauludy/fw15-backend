2b0bf22762c6f0d324ad184a0853f2d349c4ef29
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-04-17 14:15:44 +0700

    Add controller for forgot password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 const userModel = require("../models/users.model");
+const profileModel = require("../models/profile.model");
+const forgotRequestModel = require("../models/forgotRequest.model");
 const errorHandler = require("../helpers/errorHandler.helper");
 const jwt = require("jsonwebtoken");
 const { APP_SECRET } = process.env;
@@ -28,7 +30,7 @@ exports.login = async (req, res) => {
 
 exports.register = async (req, res) => {
   try {
-    const { password, confirmPassword } = req.body;
+    const { fullName, password, confirmPassword } = req.body;
     if (password !== confirmPassword) {
       throw Error("password_unmatch");
     }
@@ -38,11 +40,42 @@ exports.register = async (req, res) => {
       password: hash,
     };
     const user = await userModel.insert(data);
+    const profileData = {
+      fullName,
+      userId: user.id,
+    };
+    await profileModel.insert(profileData);
     const token = jwt.sign({ id: user.id }, APP_SECRET);
     return res.json({
       success: true,
       message: "Register success!",
       results: { token },
+    });
+  } catch (err) {
+    return errorHandler(res, err);
+  }
+};
+
+exports.forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await userModel.findOneByEmail(email);
+    if (!user) {
+      throw Error("no_user");
+    }
+    const randomNumber = Math.random();
+    const rounded = Math.round(randomNumber * 100000);
+    const padded = String(rounded).padEnd(6, "0");
+    const forgot = await forgotRequestModel.insert({
+      email: user.email,
+      code: padded,
+    });
+    if (!forgot) {
+      throw Error("forgot_failed");
+    }
+    return res.json({
+      success: true,
+      message: "Request reset password success!",
     });
   } catch (err) {
     return errorHandler(res, err);
