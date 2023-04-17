57d581c0aa7b201b89ff00bfcdebe7939afb408f
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-04-17 15:56:49 +0700

    Add validate reset password

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -5,5 +5,10 @@ const validate = require("../middlewares/validator.middleware");
 authRouter.post("/login", validate("authLogin"), authController.login);
 authRouter.post("/register", authController.register);
 authRouter.post("/forgotPassword", authController.forgotPassword);
+authRouter.post(
+  "/resetPassword",
+  validate("resetPassword"),
+  authController.resetPassword
+);
 
 module.exports = authRouter;
