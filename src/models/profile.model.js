3cea4ddcdcd819f4b9a2d4548cbc15f3731c8a5d
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-04-18 15:44:30 +0700

    Adding findOneByUserId

--- src/models/profile.model.js
+++ src/models/profile.model.js
@@ -30,6 +30,15 @@ exports.findOne = async (id) => {
   return rows[0];
 };
 
+exports.findOneByUserId = async (userId) => {
+  const query = `
+  SELECT * FROM "${table}" WHERE "userId"=$1
+  `;
+  const values = [userId];
+  const { rows } = await db.query(query, values);
+  return rows[0];
+};
+
 exports.findOneByName = async (fullName) => {
   const query = `
   SELECT * FROM "${table}" WHERE fullName=$1
@@ -84,6 +93,34 @@ exports.update = async (id, data) => {
     data.nasionality,
     data.birthDate,
     data.userId,
+  ];
+  const { rows } = await db.query(query, values);
+  return rows[0];
+};
+
+exports.updateByEmail = async (userId, data) => {
+  const query = `
+  UPDATE "${table}" 
+  SET 
+  "picture"=COALESCE(NULLIF($2, ''), "picture"),
+  "fullName"=COALESCE(NULLIF($3, ''), "fullName"),
+  "phoneNumber"=COALESCE(NULLIF($4, ''), "phoneNumber"),
+  "gender"=COALESCE(NULLIF($5, FALSE), "gender"),
+  "profession"=COALESCE(NULLIF($6, ''), "profession"),
+  "nasionality"=COALESCE(NULLIF($7, ''), "nasionality"),
+  "birthDate"=COALESCE(NULLIF($8::DATE, NULL), "birthDate")
+  WHERE "userId"=$1
+  RETURNING *;
+  `;
+  const values = [
+    userId,
+    data.picture,
+    data.fullName,
+    data.phoneNumber,
+    data.gender,
+    data.profession,
+    data.nasionality,
+    data.birthDate,
   ];
   const { rows } = await db.query(query, values);
   return rows[0];
