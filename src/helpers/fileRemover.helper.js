a847f544200045fb242e8f04e2fd1db96d39ef58
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-04-18 14:44:34 +0700

    Fixing typo

--- src/helpers/fileRemover.helper.js
+++ src/helpers/fileRemover.helper.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 const fileRemover = (file) => {
   if (file) {
-    const filename = `uploads/${fie.filename}`;
+    const filename = `uploads/${file.filename}`;
     fs.unlink(filename, (err) => {
       if (err) {
         throw Error(err.message);
