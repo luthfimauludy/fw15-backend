516ba2271f281e3cbbbbfbc9f2d9eae3019f67e8
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-04-18 13:38:31 +0700

    Adding validate on register and forgot password

--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -3,8 +3,12 @@ const authController = require("../controllers/auth.controller");
 const validate = require("../middlewares/validator.middleware");
 
 authRouter.post("/login", validate("authLogin"), authController.login);
-authRouter.post("/register", authController.register);
-authRouter.post("/forgotPassword", authController.forgotPassword);
+authRouter.post("/register", validate("authRegister"), authController.register);
+authRouter.post(
+  "/forgotPassword",
+  validate("authForgotPassword"),
+  authController.forgotPassword
+);
 authRouter.post(
   "/resetPassword",
   validate("resetPassword"),
