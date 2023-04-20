36fdd7f2d9d7da14b67488078564bbc7ff6c2401
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-04-20 21:16:10 +0700

    Adding update validator

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -3,14 +3,21 @@ const { body, query, param, validationResult } = require("express-validator");
 const nameFormat = body("name")
   .notEmpty()
   .withMessage("Name cannot be empty")
-  .isLength({ min: 3, max: 255 })
-  .withMessage("Name length is not valid");
+  .isLength({ min: 3 })
+  .withMessage("Name length is not valid, at least 3 characters");
+
+const nameUpdateFormat = body("name")
+  .optional()
+  .notEmpty()
+  .withMessage("Name cannot be empty")
+  .isLength({ min: 3 })
+  .withMessage("Name length is not valid, at least 3 characters");
 
 const fullNameFormat = body("fullName")
   .notEmpty()
   .withMessage("Full name cannot be empty")
   .isLength({ min: 3, max: 80 })
-  .withMessage("Full name length must be at least 3 characters");
+  .withMessage("Full name length is not valid, at least 3 characters");
 
 const emailFormat = body("email")
   .notEmpty()
@@ -18,7 +25,23 @@ const emailFormat = body("email")
   .isEmail()
   .withMessage("Email is not valid");
 
+const emailUpdateFormat = body("email")
+  .optional()
+  .notEmpty()
+  .withMessage("Email cannot be empty")
+  .isEmail()
+  .withMessage("Email is not valid");
+
 const passwordFormat = body("password")
+  .notEmpty()
+  .withMessage("Password cannot be empty")
+  .isStrongPassword()
+  .withMessage(
+    "Password must be strong, at least 8 characters and must include capital letters, numbers and symbols"
+  );
+
+const passwordUpdateFormat = body("password")
+  .optional()
   .notEmpty()
   .withMessage("Password cannot be empty")
   .isStrongPassword()
@@ -38,12 +61,22 @@ const queryFormat = query("sortBy")
   .withMessage("Sort type is not valid");
 
 const eventIdFormat = body("eventId")
-  .isLength({ min: 5 })
-  .withMessage("EventId is not valid");
+  .notEmpty()
+  .withMessage("EventId cannot be empty");
+
+const eventIdUpdateFormat = body("eventId")
+  .optional()
+  .notEmpty()
+  .withMessage("EventId cannot be empty");
 
 const userIdFormat = body("userId")
-  .isLength({ min: 5 })
-  .withMessage("UserId is not valid");
+  .notEmpty()
+  .withMessage("UserId cannot be empty");
+
+const userIdUpdateFormat = body("userId")
+  .optional()
+  .notEmpty()
+  .withMessage("UserId cannot be empty");
 
 const rules = {
   authLogin: [emailFormat, passwordFormat],
@@ -55,45 +88,74 @@ const rules = {
   ],
   authForgotPassword: [emailFormat],
   getAllCategories: [queryFormat],
-  getAllCities: [queryFormat],
-  getAllEventCategories: [queryFormat],
-  getAllEvents: [queryFormat],
-  getAllPartners: [queryFormat],
-  getAllPaymentMethods: [queryFormat],
-  getAllProfiles: [queryFormat],
-  getAllReservations: [queryFormat],
-  getAllReservationSections: [queryFormat],
-  getAllReservationStatus: [queryFormat],
-  getAllReservationTickets: [queryFormat],
-  getAllWishlists: [queryFormat],
-  getAllUsers: [queryFormat],
   createCategory: [nameFormat],
+  updateCategory: [nameFormat],
+  getAllCities: [queryFormat],
   createCity: [nameFormat],
-  createEventCategory: [
-    eventIdFormat,
-    body("categoryId")
-      .isLength({ min: 5 })
-      .withMessage("CategoryId is not valid"),
-  ],
+  updateCity: [nameUpdateFormat],
+  getAllEvents: [queryFormat],
   createEvent: [
     body("title")
       .notEmpty()
       .withMessage("Title cannot be empty")
-      .isLength({ min: 3, max: 255 })
+      .isLength({ min: 3 })
       .withMessage("Title length is not valid"),
-    body("date").notEmpty().withMessage("Date cannot be empty"),
-    body("cityId").isLength({ min: 5 }).withMessage("CityId is not valid"),
+    body("date")
+      .notEmpty()
+      .withMessage("Date cannot be empty")
+      .isDate({ format: "MM-DD-YYYY" })
+      .withMessage("Date format is not valid, use MM-DD-YYYY"),
+    body("cityId").notEmpty().withMessage("CityId cannot be empty"),
     body("descriptions").notEmpty().withMessage("Descriptions cannot be empty"),
   ],
+  updateEvent: [
+    body("title")
+      .optional()
+      .notEmpty()
+      .withMessage("Title cannot be empty")
+      .isLength({ min: 3 })
+      .withMessage("Title length is not valid, at least 3 characters"),
+    body("date")
+      .optional()
+      .notEmpty()
+      .withMessage("Date cannot be empty")
+      .isDate({ format: "MM-DD-YYYY" })
+      .withMessage("Date format is not valid, use MM-DD-YYYY"),
+    body("cityId").optional().notEmpty().withMessage("CityId cannot be empty"),
+    body("descriptions")
+      .optional()
+      .notEmpty()
+      .withMessage("Descriptions cannot be empty"),
+  ],
+  getAllEventCategories: [queryFormat],
+  createEventCategory: [
+    eventIdFormat,
+    body("categoryId").notEmpty().withMessage("CategoryId cannot be empty"),
+  ],
+  updateEventCategory: [
+    eventIdUpdateFormat,
+    body("categoryId")
+      .optional()
+      .notEmpty()
+      .withMessage("CategoryId cannot be empty"),
+  ],
+  getAllPartners: [queryFormat],
   createPartner: [nameFormat],
+  updatePartner: [nameUpdateFormat],
+  getAllPaymentMethods: [queryFormat],
   createPaymentMethod: [nameFormat],
+  updatePaymentMethod: [nameUpdateFormat],
+  getAllProfiles: [queryFormat],
   createProfile: [
     fullNameFormat,
     body("phoneNumber")
       .notEmpty()
       .withMessage("Phone Number cannot be empty")
+      .toInt()
+      .isNumeric()
+      .withMessage("Must be a number")
       .isLength({ min: 10, max: 12 })
-      .withMessage("Phone Number length is not valid"),
+      .withMessage("Phone Number length is not valid, at least 10 characters"),
     body("gender").notEmpty().withMessage("Gender cannot be empty"),
     body("profession")
       .notEmpty()
@@ -105,39 +167,134 @@ const rules = {
       .withMessage("Nasionality cannot be empty")
       .isLength({ min: 2 })
       .withMessage("Nasionality length is not valid"),
-    body("birthDate").notEmpty().withMessage("Birth date cannot be empty"),
+    body("birthDate")
+      .notEmpty()
+      .withMessage("Birth date cannot be empty")
+      .isDate({ format: "MM-DD-YYYY" })
+      .withMessage("Birth date format is not valid, use MM-DD-YYYY"),
   ],
+  updateProfile: [
+    body("fullName")
+      .optional()
+      .notEmpty()
+      .withMessage("Full name cannot be empty")
+      .isLength({ min: 3, max: 80 })
+      .withMessage("Full name length is not valid, at least 3 characters"),
+    body("phoneNumber")
+      .optional()
+      .notEmpty()
+      .withMessage("Phone Number cannot be empty")
+      .toInt()
+      .isNumeric()
+      .withMessage("Must be a number")
+      .isLength({ min: 10, max: 12 })
+      .withMessage("Phone Number length is not valid, at least 10 characters"),
+    body("gender").optional().notEmpty().withMessage("Gender cannot be empty"),
+    body("profession")
+      .optional()
+      .notEmpty()
+      .withMessage("Profession cannot be empty")
+      .isLength({ min: 2 })
+      .withMessage("Profession length is not valid"),
+    body("nasionality")
+      .optional()
+      .notEmpty()
+      .withMessage("Nasionality cannot be empty")
+      .isLength({ min: 2 })
+      .withMessage("Nasionality length is not valid"),
+    body("birthDate")
+      .optional()
+      .notEmpty()
+      .withMessage("BirthDate cannot be empty"),
+  ],
+  getAllReservations: [queryFormat],
   createReservation: [
     eventIdFormat,
     userIdFormat,
-    body("statusId").isLength({ min: 5 }).withMessage("StatusId is not valid"),
+    body("statusId").notEmpty().withMessage("StatusId cannot be empty"),
     body("paymentMethodId")
-      .isLength({ min: 5 })
-      .withMessage("PaymentMethodId is not valid"),
+      .notEmpty()
+      .withMessage("PaymentMethodId cannot be empty"),
   ],
+  updateReservation: [
+    eventIdUpdateFormat,
+    userIdUpdateFormat,
+    body("statusId")
+      .optional()
+      .notEmpty()
+      .withMessage("StatusId cannot be empty"),
+    body("paymentMethodId")
+      .optional()
+      .notEmpty()
+      .withMessage("PaymentMethodId cannot be empty"),
+  ],
+  getAllReservationSections: [queryFormat],
   createReservationSection: [
     nameFormat,
-    body("price").notEmpty().withMessage("Price cannot be empty"),
+    body("price")
+      .notEmpty()
+      .withMessage("Price cannot be empty")
+      .toInt()
+      .isNumeric()
+      .withMessage("Must be a number"),
   ],
+  updateReservationSection: [
+    nameUpdateFormat,
+    body("price")
+      .optional()
+      .notEmpty()
+      .withMessage("Price cannot be empty")
+      .toInt()
+      .isNumeric()
+      .withMessage("Must be a number"),
+  ],
+  getAllReservationStatus: [queryFormat],
   createReservationStatus: [nameFormat],
+  updateReservationStatus: [nameUpdateFormat],
+  getAllReservationTickets: [queryFormat],
   createReservationTicket: [
     body("reservationId")
-      .isLength({ min: 5 })
-      .withMessage("ReservationId is not valid"),
-    body("sectionId")
-      .isLength({ min: 5 })
-      .withMessage("SectionId is not valid"),
+      .notEmpty()
+      .withMessage("ReservationId cannot be empty"),
+    body("sectionId").notEmpty().withMessage("SectionId cannot be empty"),
     body("quantity").notEmpty().withMessage("Quantity cannot be empty"),
   ],
+  updateReservationTicket: [
+    body("reservationId")
+      .optional()
+      .notEmpty()
+      .withMessage("ReservationId cannot be empty"),
+    body("sectionId")
+      .optional()
+      .notEmpty()
+      .withMessage("SectionId cannot be empty"),
+    body("quantity")
+      .optional()
+      .notEmpty()
+      .withMessage("Quantity cannot be empty"),
+  ],
+  getAllWishlists: [queryFormat],
   createWishlist: [eventIdFormat, userIdFormat],
+  updateWishlist: [eventIdUpdateFormat, userIdUpdateFormat],
+  getAllUsers: [queryFormat],
   createUser: [
     body("username")
       .notEmpty()
       .withMessage("Username cannot be empty")
       .isLength({ min: 3, max: 80 })
-      .withMessage("Username length is not valid"),
+      .withMessage("Username length is not valid, at least 3 characters"),
     emailFormat,
     passwordFormat,
+  ],
+  updateUser: [
+    body("username")
+      .optional()
+      .notEmpty()
+      .withMessage("Username cannot be empty")
+      .isLength({ min: 3, max: 80 })
+      .withMessage("Username length is not valid, at least 3 characters"),
+    emailUpdateFormat,
+    passwordUpdateFormat,
   ],
   idParams: [
     param("id")
