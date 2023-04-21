80cedc83a07eb6bf09113a26e3b56db61fb57746
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-04-21 20:59:41 +0700

    Adding profile controller

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -2,25 +2,44 @@ const profileModel = require("../models/profile.model");
 const fileRemover = require("../helpers/fileRemover.helper");
 const errorHandler = require("../helpers/errorHandler.helper");
 
-exports.updateProfile = async (req, res) => {
-  const { id } = req.user;
-  const user = await profileModel.findOneByUserId(id);
-  const data = {
-    ...req.body,
-  };
-  if (req.file) {
-    if (user.picture) {
-      fileRemover({ filename: user.picture });
+exports.getProfile = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const profile = await profileModel.findOneByUserId(id);
+    if (!profile) {
+      return errorHandler(res, undefined);
     }
-    data.picture = req.file.filename;
+    return res.json({
+      success: true,
+      message: "Profile",
+      results: profile,
+    });
+  } catch (err) {
+    return errorHandler(res, err);
   }
-  const profile = await profileModel.updateByEmail(id, data);
-  if (!profile) {
-    return errorHandler(res, undefined);
+};
+
+exports.updateProfile = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const user = await profileModel.findOneByUserId(id);
+    const data = { ...req.body };
+    if (req.file) {
+      if (user.picture) {
+        fileRemover({ filename: user.picture });
+      }
+      data.picture = req.file.filename;
+    }
+    const profile = await profileModel.updateByEmail(id, data);
+    if (!profile) {
+      return errorHandler(res, undefined);
+    }
+    return res.json({
+      success: true,
+      message: "Profile updated",
+      results: profile,
+    });
+  } catch (err) {
+    return errorHandler(res, err);
   }
-  return res.json({
-    success: true,
-    message: "Profile updated",
-    results: profile,
-  });
 };
