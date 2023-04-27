dd22619855c5e4cfdffaa2d10a9615548b6cb0f9
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-04-27 22:52:57 +0700

    Adding findOneByUserId

--- src/models/wishlists.model.js
+++ src/models/wishlists.model.js
@@ -5,18 +5,18 @@ const table = "wishlists";
 exports.findAll = async (qs) => {
   page = parseInt(qs.page) || 1;
   limit = parseInt(qs.limit) || 5;
-  // search = qs.search || "";
+  search = qs.search || "";
   sort = qs.sort || "id";
   sortBy = qs.sortBy || "ASC";
 
   const offset = (page - 1) * limit;
   const query = `
   SELECT * FROM "${table}"
-  WHERE "eventId"=$3
+  WHERE "id"::TEXT LIKE $3
   ORDER BY ${sort} ${sortBy}
   LIMIT $1 OFFSET $2
   `;
-  const values = [limit, offset, eventId];
+  const values = [limit, offset, `%${search}%`];
   const { rows } = await db.query(query, values);
   return rows;
 };
@@ -26,6 +26,23 @@ exports.findOne = async (id) => {
   SELECT * FROM "${table}" WHERE id=$1
   `;
   const values = [id];
+  const { rows } = await db.query(query, values);
+  return rows[0];
+};
+
+exports.findOneByUserId = async (userId) => {
+  const query = `
+  SELECT 
+  "events"."id",
+  "users"."id",
+  "${table}"."createdAt",
+  "${table}"."updatedAt"
+  FROM "${table}" 
+  JOIN "events" ON "events"."id" = "${table}"."eventId"
+  JOIN "users" ON "users"."id" = "${table}"."userId"
+  WHERE "${table}"."userId"=$1
+  `;
+  const values = [userId];
   const { rows } = await db.query(query, values);
   return rows[0];
 };
