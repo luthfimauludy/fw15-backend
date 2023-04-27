c88905972be335b26ea20614d2330a1ac43d43d2
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-04-27 22:55:40 +0700

    Adding change password and wishlist route

--- src/routers/index.js
+++ src/routers/index.js
@@ -11,16 +11,17 @@ router.get("/", (req, res) => {
 router.use("/auth", require("./auth.router"));
 router.use("/admin", authMiddleware, require("./admin/admin.router"));
 router.use("/categories", authMiddleware, require("./categories.router"));
-router.use("/cities", authMiddleware, require("./cities.router"));
-router.use("/events", authMiddleware, require("./events.router"));
-router.use("/partners", authMiddleware, require("./partners.router"));
-router.use("/profile", authMiddleware, require("./profile.router"));
-router.use("/reservations", authMiddleware, require("./reservations.router"));
 router.use(
   "/change-password",
   authMiddleware,
   require("./changePassword.router")
 );
+router.use("/cities", authMiddleware, require("./cities.router"));
+router.use("/events", authMiddleware, require("./events.router"));
+router.use("/partners", authMiddleware, require("./partners.router"));
+router.use("/profile", authMiddleware, require("./profile.router"));
+router.use("/reservations", authMiddleware, require("./reservations.router"));
+router.use("/wishlists", authMiddleware, require("./wishlists.router"));
 
 router.use("*", (req, res) => {
   return res.status(404).json({
