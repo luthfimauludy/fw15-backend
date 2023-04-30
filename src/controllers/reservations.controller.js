6e487646dc0c18e73b9134309f17dcf448b60be7
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-04-30 23:38:45 +0700

    feat: create reservation

--- src/controllers/reservations.controller.js
+++ src/controllers/reservations.controller.js
@@ -1,13 +1,24 @@
 const reservationsModel = require("../models/reservations.model");
+const reservationTicketsModel = require("../models/reservationTickets.model");
 const errorHandler = require("../helpers/errorHandler.helper");
 
 exports.createReservation = async (req, res) => {
   try {
-    const data = { ...req.body };
-    const reservation = await reservationsModel.insert(data);
+    const { id } = req.user;
+    const { eventId, sectionId, quantity } = req.body;
+    const reservationData = {
+      eventId,
+      userId: id,
+    };
+    const reservation = await reservationsModel.insert(reservationData);
+    const reservationTicketData = {
+      sectionId,
+      quantity,
+    };
+    await reservationTicketsModel.insert(reservationTicketData);
     return res.json({
       success: true,
-      message: "Reservation created",
+      message: "Create reservation success!",
       results: reservation,
     });
   } catch (err) {
