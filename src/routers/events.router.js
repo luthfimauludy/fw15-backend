331f5b47394c8efc402163071a8c428aee067d72
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-05-01 23:29:52 +0700

    feat: new route create manage event

--- src/routers/events.router.js
+++ src/routers/events.router.js
@@ -1,7 +1,14 @@
 const eventRouter = require("express").Router();
 const eventController = require("../controllers/events.controller");
+const uploadMiddleware = require("../middlewares/upload.middleware");
 
 eventRouter.get("/", eventController.getAllEvents);
 eventRouter.get("/:id", eventController.getOneEvent);
+eventRouter.post(
+  "/manage",
+  uploadMiddleware("picture"),
+  eventController.createManageEvent
+);
+// eventRouter.patch("/manage", eventController.updateManageEvent);
 
 module.exports = eventRouter;
