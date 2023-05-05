70089ac7fa47e12228fc65e9e3a40ab6e6c11ddd
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-05-06 00:10:33 +0700

    feat: installing cors

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ require("dotenv").config({
 });
 
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.use("/uploads", express.static("uploads"));
 
