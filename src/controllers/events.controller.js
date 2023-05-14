74f6544b7b634edb677d743743f4073bc822b1ac
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-05-14 23:53:12 +0700

    fix: get one event and by user login

--- src/controllers/events.controller.js
+++ src/controllers/events.controller.js
@@ -5,7 +5,7 @@ const errorHandler = require("../helpers/errorHandler.helper");
 exports.getAllEvents = async (req, res) => {
   try {
     const data = { ...req.query };
-    const events = await eventsModel.findAll(data);
+    const events = await eventsModel.findAllByUserLogin(data);
     return res.json({
       success: true,
       message: "List of all events",
@@ -16,29 +16,29 @@ exports.getAllEvents = async (req, res) => {
   }
 };
 
-exports.getDetailEvents = async (req, res) => {
+exports.getOneEvent = async (req, res) => {
   try {
-    const { id } = req.params;
-    const event = await eventsModel.findOne(id);
+    const data = await eventsModel.findOne(req.params.id);
+    if (!data) {
+      return errorHandler(res, undefined);
+    }
     return res.json({
       success: true,
-      message: "List of all user events",
-      results: event,
+      message: "Event",
+      results: data,
     });
   } catch (err) {
     return errorHandler(res, err);
   }
 };
 
-exports.getOneEvent = async (req, res) => {
+exports.getDetailEventsByUserLogin = async (req, res) => {
   try {
-    const event = await eventCategoriesModel.findOneByEventId(req.params.id);
-    if (!event) {
-      return errorHandler(res, undefined);
-    }
+    const { id } = req.params;
+    const event = await eventsModel.findEventsByUserLogin(id);
     return res.json({
       success: true,
-      message: "Event",
+      message: "List of all user events",
       results: event,
     });
   } catch (err) {
@@ -69,13 +69,15 @@ exports.createManageEvent = async (req, res) => {
 
 exports.updateManageEvent = async (req, res) => {
   try {
-    const user = await eventsModel.findOneById(req.params.id);
+    const { id } = req.user;
+    const user = await eventsModel.findOneById(id);
     const data = { ...req.body };
     if (req.file) {
       if (user.picture) {
         fileRemover({ filename: user.picture });
       }
-      data.picture = req.file.filename;
+      // data.picture = req.file.filename;
+      data.picture = req.file.path;
     }
     const event = await eventsModel.update(req.params.id, data);
     if (!event) {
