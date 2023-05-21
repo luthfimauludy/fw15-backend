fa354a6984b182e23310d794325e5be2cd62ea02
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-05-21 17:13:15 +0700

    feat: get all payments route

--- src/routers/payments.router.js
+++ src/routers/payments.router.js
@@ -1,6 +1,7 @@
 const paymentsRouter = require("express").Router();
 const paymentsController = require("../controllers/payments.controller");
 
+paymentsRouter.get("/", paymentsController.getAllPayment);
 paymentsRouter.post("/", paymentsController.createPayment);
 
 module.exports = paymentsRouter;
