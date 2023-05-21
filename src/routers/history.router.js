db9c37b1e1b79f43d160e933df2177bbac5adcd5
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-05-21 23:43:51 +0700

    add router getAllHistory

--- src/routers/history.router.js
+++ src/routers/history.router.js
@@ -1,6 +1,6 @@
 const historyRouter = require("express").Router();
 const historyController = require("../controllers/history.controller");
 
-historyRouter.get("/", historyController.getHistory);
+historyRouter.get("/", historyController.getAllHistory);
 
 module.exports = historyRouter;
