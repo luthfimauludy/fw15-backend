ef22716eb11e48a92c8e9747c7dad17efcd81dff
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2023-05-21 23:41:29 +0700

    add findAllByUserId

--- src/models/reservations.model.js
+++ src/models/reservations.model.js
@@ -21,6 +21,35 @@ exports.findAll = async (qs) => {
   return rows;
 };
 
+exports.findAllByUserId = async (id, qs) => {
+  page = parseInt(qs.page) || 1;
+  limit = parseInt(qs.limit) || 5;
+  search = qs.search || "";
+  sort = (qs.sort && `"events"."${qs.sort}"`) || '"reservations"."id"';
+  sortBy = qs.sortBy || "DESC";
+  city = qs.city || "";
+
+  const offset = (page - 1) * limit;
+  const query = `
+  SELECT
+  "${table}"."id",
+  "events"."title",
+  "cities"."name" as "location",
+  "events"."date"
+  FROM "${table}"
+  JOIN "events" ON "events"."id" = "${table}"."eventId"
+  JOIN "cities" ON "cities"."id" = "events"."cityId"
+  WHERE "${table}"."userId" = $5
+  AND "events"."title" LIKE $3
+  AND "cities"."name" LIKE $4
+  ORDER BY ${sort} ${sortBy}
+  LIMIT $1 OFFSET $2
+  `;
+  const values = [limit, offset, `%${search}%`, `%${city}%`, id];
+  const { rows } = await db.query(query, values);
+  return rows;
+};
+
 exports.findOne = async (id) => {
   const query = `
   SELECT * FROM "${table}" WHERE id=$1
