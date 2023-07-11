2451d5ca51a41c17e3b99fba48573e886342dfe0
luthfimauludy <putra.luthfi@gmail.com>
2023-07-11 13:16:17 +0700

    update: proccess dotenv to firebase credentials

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,6 +1,7 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("../../eventix-app-firebase-adminsdk-9yvz3-32bc025d7f.json");
+// var serviceAccount = require("../../eventix-app-firebase-adminsdk-9yvz3-32bc025d7f.json");
+var serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
